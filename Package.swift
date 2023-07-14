d701208e823303235e16d5370fca05b4309f1f53
StorifyMe Team <139544247+storifyme-team@users.noreply.github.com>
2023-07-15 01:54:12 +0200

    Update Package.swift for 1.0.11

--- Package.swift
+++ Package.swift
@@ -1,14 +1,25 @@
-<!DOCTYPE html>
-<html lang="en">
+// swift-tools-version:5.3
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>StorifyMe</title>
-</head>
+import PackageDescription
 
-<body>
-</body>
-
-</html>
+let package = Package(
+    name: "StorifyMeSnaps",
+    platforms: [
+        .iOS(.v11)
+    ],
+    products: [
+        .library(
+            name: "StorifyMeSnaps",
+            targets: ["StorifyMeSnaps"]
+        )
+    ],
+    dependencies: [],
+    targets: [
+        .binaryTarget(
+            name: "StorifyMeSnaps",
+            url: "https://sdk.storifyme.com/ios/1.0.11/StorifyMeSnaps.zip",
+            checksum: "3c10771bafd559ebda5c8bc113fd35b98a08e21f5eddcc4b671b17ccb9f0007e"
+        )
+    ],
+    swiftLanguageVersions: [.v5]
+)
