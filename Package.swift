db9ad124fc8efb89bbb45ff7b8addc40c9f21db1
StorifyMe Team <contact@storifyme.com>
2023-07-14 23:37:32 +0000

    Version 1.0.11

--- Package.swift
+++ Package.swift
@@ -1,28 +1,14 @@
-// swift-tools-version:5.3
+<!DOCTYPE html>
+<html lang="en">
 
-import PackageDescription
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>StorifyMe</title>
+</head>
 
-let package = Package(
-    name: "StorifyMeSnaps",
-    platforms: [
-        .iOS(.v11)
-    ],
-    products: [
-        .library(
-            name: "StorifyMeSnaps",
-            targets: ["StorifyMeSnaps"]
-        )
-    ],
-    dependencies: [],
-    targets: [
-        .binaryTarget(
-            name: "StorifyMeSnaps",
-            url: "https://sdk.dev.storifyme.com/ios/1.0.10/StorifyMeSnaps.zip",
-            checksum: "a04a8c18bc89981f1317681b8ca9fc2b1ef5d95dc29ed51808288d526c3800b2"
-        ),
-        resources: [
-            .copy("StorifyMeSnaps.framework/EditorSDK")
-        ]
-    ],
-    swiftLanguageVersions: [.v5]
-)
+<body>
+</body>
+
+</html>
